f90f164b747e4f1c12ee4cd5f6bd64ce1644ec2f
Joao Paulo Reis Alvarenga <joaopaulo.reisalvarenga@gmail.com>
2022-01-29 00:04:04 -0300

    feat: add combining datasets

---
+++ combine_datasets.py
@@ -0,0 +1,35 @@
+import argparse
+import itertools as it
+import glob
+import os
+
+from tqdm import tqdm
+
+
+def load_file(filename):
+    with open(filename) as f:
+        data = set([l.strip() for l in f])
+    return data
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='Clean dataset')
+    parser.add_argument("--path", type=str, required=True,
+                        help="Normalized dataset folder")
+    parser.add_argument('--output', type=str, required=True, help='Combinations output folder')
+    args = parser.parse_args()
+
+    files = glob.glob(os.path.join(args.path, '*'))
+    for i in range(len(files)):
+        combinations = list(it.combinations(files, i + 1))
+        print('Combining', i + 1, 'Total combinations:', len(combinations))
+        for combination in tqdm(combinations, total=len(combinations)):
+            filenames = sorted([os.path.basename(c) for c in combination])
+            data = set()
+            final_name = '-'.join([name.replace('.txt', '') for name in filenames])
+            for filename in filenames:
+                data = data.union(load_file(os.path.join(args.path, filename)))
+            with open(os.path.join(args.output, f'{final_name}.txt'), 'w') as f:
+                f.write('\n'.join(data))
+
+
