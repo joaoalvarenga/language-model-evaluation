c95183af24aeb21f0bd73406bd2d52c67d6fe988
Joao Paulo Reis Alvarenga <joaopaulo.reisalvarenga@gmail.com>
2022-02-16 09:36:37 -0300

    feat: add script to evaluate all lm

--- evaluate_hf_kenlm.sh
+++ evaluate_hf_kenlm.sh
@@ -7,8 +7,8 @@ NC='\033[0m' # No Color
 
 echo ""
 for n in 3 4 5; do
-  for filename in "$MODELS_FOLDER/$n-gram/*.arpa"; do
-    echo "${GREEN}Evaluating $filename...${NC}"
+  for filename in $MODELS_FOLDER/$n-gram/*.arpa; do
+    echo -e "${GREEN}Evaluating $filename...${NC}"
     python3 evaluate_language_model.py --hypothesis_path $HYPOTHESIS_PATH --lm_model_name $filename --output $OUTPUT_FOLDER
   done
 done
